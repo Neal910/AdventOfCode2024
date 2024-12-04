38c01916e6065cba59d493bbaba04df44ee29a46
Feng Sha <feng.sha@fullscript.com>
2024-12-04 09:08:36 -0500

    day4 Aoc

--- util/util.rb
+++ util/util.rb
@@ -7,4 +7,17 @@ class Util
     end
     list
   end
+
+  def self.parse_letters_to_2d_array(file_path)
+    result = []
+
+    File.foreach(file_path).with_index do |line, row_index|
+      line.strip.chars.each_with_index do |char, col_index|
+        result[row_index] ||= []
+        result[row_index][col_index] = char
+      end
+    end
+
+    result
+  end
 end
